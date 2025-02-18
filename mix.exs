5de3f7e1a1371378b7ddd208a915aac03630dc62
Gwen W <gwendolyn.whelan@gmail.com>
2025-02-18 12:42:34 -0500

    rename project and update defconfigs

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi3a.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3a
+  @github_organization "electra-research"
+  @app :electra_rpi3a
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
@@ -68,7 +68,7 @@ defmodule NervesSystemRpi3a.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.11", runtime: false},
-      {:nerves_system_br, "1.29.3", runtime: false},
+      {:nerves_system_br, git: "https://github.com/nerves-project/nerves_system_br", runtime: false, override: true},
       {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 13.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
