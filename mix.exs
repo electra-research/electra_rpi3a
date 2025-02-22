727aca4fb3330ad52adce12d7853e0758fe85b85
Gwen W <gwendolyn.whelan@gmail.com>
2025-02-22 15:56:44 -0500

    rename

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi3a.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3a
+  @github_organization "electra-research"
+  @app :electra_rpi3a
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
